4624eed4ac3c974c39ce06408bde7ac2acc65f65
Terpavor <vvv.200@mail.ru>
2019-02-13 05:55:21 +0300

    step 2

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf $SCRIPT_PATH/etc/nginx.conf \
+            /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+
+mkdir -p /etc/gunicorn.d
+sudo ln -sf $SCRIPT_PATH/etc/gunicorn.conf \
+            /etc/gunicorn.d/test
+gunicorn3                   \
+    --chdir $SCRIPT_PATH    \
+    --bind 0.0.0.0:8080     \
+    hello:app
+# sudo /etc/init.d/gunicorn restart
