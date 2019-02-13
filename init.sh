17ffef39b6030d1c90f1bf8b9a1fe53a64f76d1a
Terpavor <vvv.200@mail.ru>
2019-02-13 06:12:05 +0300

    step 1 issue

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
-SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+cd `dirname $0`
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf $SCRIPT_PATH/etc/nginx.conf \
+sudo ln -sf etc/nginx.conf \
             /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 
 mkdir -p /etc/gunicorn.d
-sudo ln -sf $SCRIPT_PATH/etc/gunicorn.conf \
+sudo ln -sf etc/gunicorn.conf \
             /etc/gunicorn.d/test
 gunicorn3                   \
-    --chdir $SCRIPT_PATH    \
     --bind 0.0.0.0:8080     \
     hello:app
 # sudo /etc/init.d/gunicorn restart
+#    --chdir "$0"            \
