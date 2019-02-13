5934b4b3a8b0da5367042cf3b2d2dc5ab101aa94
Terpavor <vvv.200@mail.ru>
2019-02-13 06:27:43 +0300

    step 2

--- init.sh
+++ init.sh
@@ -9,8 +9,9 @@ sudo /etc/init.d/nginx restart
 mkdir -p /etc/gunicorn.d
 sudo ln -sf /home/box/web/etc/gunicorn.conf \
             /etc/gunicorn.d/test
-gunicorn3                   \
+gunicorn                    \
+    --chdir /home/box/web/  \
     --bind 0.0.0.0:8080     \
     hello:app
-# sudo /etc/init.d/gunicorn restart
-#    --chdir "$0"            \
+
+sudo /etc/init.d/gunicorn restart
