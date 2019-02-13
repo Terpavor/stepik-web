d465484cb16baee1e466f68e95aa4cef41ec3b28
Terpavor <vvv.200@mail.ru>
2019-02-13 06:21:25 +0300

    step 1 issue

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 cd `dirname $0`
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf etc/nginx.conf \
+sudo ln -sf /home/box/web/etc/nginx.conf \
             /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 
 mkdir -p /etc/gunicorn.d
-sudo ln -sf etc/gunicorn.conf \
+sudo ln -sf /home/box/web/etc/gunicorn.conf \
             /etc/gunicorn.d/test
 gunicorn3                   \
     --bind 0.0.0.0:8080     \
