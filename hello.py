3efa0f08748dc46e7cbc4874d9c78c656e7ddfff
Terpavor <vvv.200@mail.ru>
2019-02-13 06:52:50 +0300

    .py

---
+++ hello.py
@@ -0,0 +1,15 @@
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    
+    body = (
+        environ['QUERY_STRING']     #
+        .split('?')[-1]             # от последнего '?'
+        .split('#')[0]              # до первого #
+        .replace('&', '\n')         #
+    )
+    
+    start_response(status, headers)
+    return [body]
