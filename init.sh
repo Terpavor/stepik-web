77b208653935316083fd88c8980abd328a0dc735
Terpavor <vvv.200@mail.ru>
2019-02-15 10:18:28 +0300

    back to the start

--- init.sh
+++ init.sh
@@ -1,22 +1,21 @@
+# --- 1. nginx ------
 sudo ln                              `# make links between files         `\
     --symbolic                       `# symbolic link, not hard link     `\
     --force                          `# delete existing destination file `\
     /home/box/web/etc/nginx.conf     `# source file                      `\
     /etc/nginx/sites-enabled/default `# destination symlink              `
 
-#service nginx restart
-/etc/init.d/nginx restart
+sudo service nginx restart
+
+# --- 2. gunicorn ---
+sudo gunicorn               `#          				`\
+    --chdir /home/box/web/  `# to application folder	`\
+    --bind 0.0.0.0:8080     `# socket to bind         	`\
+    hello:app				`# module:function         	`
 
 exit 0
 
-mkdir -p /etc/gunicorn.d
-sudo ln -sf /home/box/web/etc/gunicorn.conf \
-            /etc/gunicorn.d/test
-gunicorn                    \
-    --chdir /home/box/web/  \
-    --bind 0.0.0.0:8080     \
-    hello:app
-
-sudo /etc/init.d/gunicorn restart
-
-exit 0
+#sudo mkdir -p /etc/gunicorn.d
+#sudo ln -sf /home/box/web/etc/gunicorn.conf \
+#            /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
