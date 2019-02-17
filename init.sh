261a99e1fcf1b3694c10224e4ff6272a43d64dc4
Terpavor <vvv.200@mail.ru>
2019-02-17 07:40:41 +0300

    ...

--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@ sudo ln                              `# make links between files         `\
     --symbolic                       `# symbolic link, not hard link     `\
     --force                          `# delete existing destination file `\
     /home/box/web/etc/nginx.conf     `# source file                      `\
-    /etc/nginx/sites-enabled/default `# destination symlink              `
+    /etc/nginx/sites-enabled/default `# destination file (symlink)       `
 
-sudo service nginx restart
+sudo nginx -s reload
+# ^ запускает ли это nginx?..
+
 
 # --- 2. gunicorn ---
 sudo gunicorn               `#          				`\
@@ -13,9 +15,32 @@ sudo gunicorn               `#          				`\
     --bind 0.0.0.0:8080     `# socket to bind         	`\
     hello:app				`# module:function         	`
 
+
+
+
+read -p "Press enter to continue..."
+
+
+
+
+
+sudo service nginx stop
 exit 0
 
+
+
+# sudo service nginx restart
+# bash "$(dirname "$0")/gunicorn_start.sh"
+
+#sudo gunicorn               `#          				`\
+#    --chdir /home/box/web/  `# to application folder	`\
+#    --bind 0.0.0.0:8080     `# socket to bind         	`\
+#    hello:app				 `# module:function         	`
+
+
+
 #sudo mkdir -p /etc/gunicorn.d
-#sudo ln -sf /home/box/web/etc/gunicorn.conf \
-#            /etc/gunicorn.d/test
+#sudo ln -sf 						\
+#	/home/box/web/etc/gunicorn.conf \
+#	/etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
